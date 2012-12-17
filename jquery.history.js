721a77f49cf580bcc053da6754a9d8d890d5617f
Riga <marcelrieger@me.com>
2012-12-17 10:03:38 +0100

    Hard -> Soft Tabs.

--- jquery.history.js
+++ jquery.history.js
@@ -9,92 +9,94 @@
  */
 
 var _history = {
-	instance: null,
-	callbacks: jQuery.Callbacks()
+    instance: null,
+    callbacks: jQuery.Callbacks()
 };
 
-jQuery.History = function( callback, append ) {
-	
-	if ( callback && (callback.fire || jQuery.isFunction( callback )) ) {
-		if ( append || append === undefined ) {
-			_history.callbacks.add( callback.fire || callback );
-		} else {
-			_history.callbacks = callback.fire ? callback : jQuery.Callbacks().add( callback );
-		}
-	}
-	
-	if ( _history.instance ) {
-		return _history.instance;
-	}
-	
-	var self,
-	
-	push = function( url, _state ) {
-		if ( !jQuery.isPlainObject( url ) ) {
-			url = jQuery.extend( true, { url: url }, _state );
-		}
-		_state = url;
-		window.history.pushState( _state, '', encodeURI( _state.url ) );
-		_callbacks().fire( _state );
-		return self;
-	},
-	
-	modify = function( _state, extend, fire ) {
-		if ( extend || extend === undefined ) {
-			_state = jQuery.extend( true, state(), _state );
-		}
-		window.history.replaceState( _state );
-		if ( fire ) {
-			_callbacks().fire( _state );
-		}
-		return self;
-	},
-	
-	forward = function() {
-		window.history.forward();
-		return self;
-	},
-	
-	back = function() {
-		window.history.back();
-		return self;
-	},
-	
-	go = function( position ) {
-		window.history.go( position );
-		return self;
-	},
-	
-	state = function() {
-		return window.history.state;
-	},
-	
-	size = function() {
-		return window.history.length;
-	},
-	
-	_callbacks = function() {
-		return _history.callbacks;
-	},
-	
-	_handle = function( event ) {
-		if ( !event || !event.originalEvent || !event.originalEvent.state ) {
-			return;
-		}
-		_callbacks().fire( event.originalEvent.state );
-	};
-	
-	$(window).on( "popstate", _handle );
-	
-	self = {
-		push: push,
-		modify: modify,
-		forward: forward,
-		back: back,
-		go: go,
-		state: state,
-		size: size
-	};
-	
-	return _history.instance = self;
+jQuery.History = function(callback, append) {
+
+    if(callback && (callback.fire || jQuery.isFunction(callback))) {
+        if(append || append === undefined) {
+            _history.callbacks.add(callback.fire || callback);
+        } else {
+            _history.callbacks = callback.fire ? callback : jQuery.Callbacks().add(callback);
+        }
+    }
+
+    if(_history.instance) {
+        return _history.instance;
+    }
+
+    var self,
+
+    push = function(url, _state) {
+        if(!jQuery.isPlainObject(url)) {
+            url = jQuery.extend(true, {
+                url: url
+            }, _state);
+        }
+        _state = url;
+        window.history.pushState(_state, '', encodeURI(_state.url));
+        _callbacks().fire(_state);
+        return self;
+    },
+
+    modify = function(_state, extend, fire) {
+        if(extend || extend === undefined) {
+            _state = jQuery.extend(true, state(), _state);
+        }
+        window.history.replaceState(_state);
+        if(fire) {
+            _callbacks().fire(_state);
+        }
+        return self;
+    },
+
+    forward = function() {
+        window.history.forward();
+        return self;
+    },
+
+    back = function() {
+        window.history.back();
+        return self;
+    },
+
+    go = function(position) {
+        window.history.go(position);
+        return self;
+    },
+
+    state = function() {
+        return window.history.state;
+    },
+
+    size = function() {
+        return window.history.length;
+    },
+
+    _callbacks = function() {
+        return _history.callbacks;
+    },
+
+    _handle = function(event) {
+        if(!event || !event.originalEvent) {
+            return;
+        }
+        _callbacks().fire(event.originalEvent.state);
+    };
+
+    $(window).on("popstate", _handle);
+
+    self = {
+        push: push,
+        modify: modify,
+        forward: forward,
+        back: back,
+        go: go,
+        state: state,
+        size: size
+    };
+
+    return _history.instance = self;
 };
