605feb995cda8b8cb1e62399fe3ad803c1162821
Riga <marcelrieger@me.com>
2013-07-27 23:48:55 +0200

    Fix push signature.

--- jquery.history.js
+++ jquery.history.js
@@ -15,44 +15,40 @@ var _history = {
 
 jQuery.History = function(callback, append) {
 
-    if(callback && (callback.fire || jQuery.isFunction(callback))) {
-        if(append || append === undefined) {
+    if (callback && (callback instanceof Function || callback.fire)) {
+        if (append)
             _history.callbacks.add(callback.fire || callback);
-        } else {
+        else
             _history.callbacks = callback.fire ? callback : jQuery.Callbacks().add(callback);
-        }
     }
 
-    if(_history.instance) {
+    if (_history.instance)
         return _history.instance;
-    }
 
     var self,
 
     _enabled = true,
 
-    push = function(url, _state, fire) {
-        if(!jQuery.isPlainObject(url)) {
-            url = jQuery.extend(true, {
-                url: url
-            }, _state);
-        }
-        _state = url;
-        window.history.pushState(_state, '', encodeURI(_state.url));
-        if(fire) {
+    push = function(url, title, _state, fire) {
+        title = title || "";
+        if (fire === undefined && _state instanceof Boolean) {
+            fire = _state;
+            _state = {};
+        } else if (_state === undefined)
+            _state = {};
+        jQuery.extend(_state, {url: url, title: title});
+        window.history.pushState(_state, title, encodeURI(url));
+        if (fire)
             _callbacks().fire(_state);
-        }
         return self;
     },
 
     modify = function(_state, extend, fire) {
-        if(extend || extend === undefined) {
-            _state = jQuery.extend(true, state(), _state);
-        }
+        if (extend)
+            _state = jQuery.extend(true, {}, state(), _state);
         window.history.replaceState(_state);
-        if(fire) {
+        if (fire)
             _callbacks().fire(_state);
-        }
         return self;
     },
 
@@ -79,8 +75,10 @@ jQuery.History = function(callback, append) {
         return window.history.length;
     },
 
-    enable = function() {
+    enable = function(fire) {
         _enabled = true;
+        if (fire)
+            _callbacks().fire();
         return self;
     },
 
@@ -108,16 +106,15 @@ jQuery.History = function(callback, append) {
     },
 
     _handle = function(event) {
-        if(!enabled() || !event || !event.originalEvent) {
-            return;
-        }
+        if(!enabled() || !event || !event.originalEvent)
+            return self;
         _callbacks().fire(event.originalEvent.state);
     };
 
-    // workaround
+    // call _handle on popstate events
     $(window).load(function() {
         window.setTimeout(function() {
-            $(window).on('popstate', _handle);
+            $(window).on('popstate', _handle.bind(self));
         }, 0);
     });
 
